22e1a0bbd5b5e4852edef28bb143656e9ec5a4e1
sauravsinha137 <k123saurav@gmail.com>
2019-10-01 17:09:35 +0530

    added aware interface

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,12 +1,19 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-public class Movie  {
-    @Autowired
+public class Movie implements ApplicationContextAware, BeanNameAware , BeanFactoryAware {
+     @Autowired
     private Actor actor;
 
-
+    private ApplicationContext app;
+    private BeanFactory factory;
     public void setActor(Actor actor) {
         this.actor = actor;
     }
@@ -19,7 +26,19 @@ public class Movie  {
         //  actor.pump();
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.app=applicationContext;
+    }
 
+    @Override
+    public void setBeanName(String s) {
+        System.out.println(s);
+    }
 
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.factory=beanFactory;
 
+    }
 }
